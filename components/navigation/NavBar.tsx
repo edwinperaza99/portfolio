b5906840cf5d833cfc84d50c866ed8e461033917
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-10-30 10:46:30 -0700

    animate navbar

--- components/navigation/NavBar.tsx
+++ components/navigation/NavBar.tsx
@@ -2,69 +2,117 @@
 import { GiHamburger } from "react-icons/gi";
 import { useState } from "react";
 import Link from "next/link";
+import { motion, AnimatePresence } from "framer-motion";
 
 const NavBar = () => {
 	const [isOpen, setIsOpen] = useState(false);
 
 	const toggleOverlay = () => setIsOpen(!isOpen);
+
+	// Variants for animation
+	const overlayVariants = {
+		open: {
+			opacity: 1,
+			transition: { when: "beforeChildren", staggerChildren: 0.15 },
+		},
+		closed: {
+			opacity: 0,
+			transition: {
+				when: "afterChildren",
+				staggerChildren: 0.1,
+				staggerDirection: -1,
+			},
+		},
+	};
+
+	const itemVariants = {
+		open: { opacity: 1, y: 0 },
+		closed: { opacity: 0, y: -20 },
+	};
+
 	return (
-		<nav className="">
+		<nav>
 			<button
 				title="menu-toggle"
 				onClick={toggleOverlay}
-				className="fixed top-4 right-4 p-2 bg-gray-800 text-white rounded"
+				disabled={isOpen}
+				className={`fixed top-4 right-4 p-2 bg-gray-800 text-white rounded ${
+					isOpen ? "cursor-not-allowed opacity-50" : ""
+				}`}
 			>
 				<GiHamburger />
 			</button>
+
 			{/* Full-Screen Overlay */}
-			{isOpen && (
-				<div className="fixed inset-0 bg-gray-800 text-white z-60 flex items-center justify-center">
-					<ul className="grid grid-cols-5 grid-rows-5 uppercase w-screen h-screen max-h-screen max-w-screen cursor-pointer">
-						<li className="col-span-3 row-span-2">
-							<Link
-								href="/"
-								className="p-4 flex justify-end items-end w-full h-full border hover:bg-gray-900"
+			<AnimatePresence>
+				{isOpen && (
+					<motion.div
+						className="fixed inset-0 text-white z-60 flex items-center justify-center"
+						initial="closed"
+						animate="open"
+						exit="closed"
+						variants={overlayVariants}
+					>
+						<motion.ul className="grid grid-cols-5 grid-rows-5 uppercase w-screen h-screen cursor-pointer">
+							<motion.li
+								variants={itemVariants}
+								className="col-span-3 row-span-2"
+							>
+								<Link
+									href="/"
+									className="p-4 flex justify-end items-end w-full h-full border bg-gray-800 hover:bg-gray-900"
+									onClick={toggleOverlay}
+								>
+									Home
+								</Link>
+							</motion.li>
+							<motion.li
+								variants={itemVariants}
+								className="col-span-2 row-span-3"
+							>
+								<Link
+									href="/about"
+									className="p-4 flex justify-start items-end w-full h-full border bg-gray-800 hover:bg-gray-900"
+									onClick={toggleOverlay}
+								>
+									About
+								</Link>
+							</motion.li>
+							<motion.li
+								variants={itemVariants}
+								className="col-span-2 row-span-3"
+							>
+								<Link
+									href="projects"
+									className="p-4 flex justify-end items-start w-full h-full border bg-gray-800 hover:bg-gray-900"
+									onClick={toggleOverlay}
+								>
+									Projects
+								</Link>
+							</motion.li>
+							<motion.li
+								variants={itemVariants}
+								className="flex justify-center items-center border cursor-pointer bg-gray-800 hover:bg-gray-900"
 								onClick={toggleOverlay}
 							>
-								Home
-							</Link>
-						</li>
-						<li className="col-span-2 row-span-3">
-							<Link
-								href="/about"
-								className="p-4 flex justify-start items-end w-full h-full border hover:bg-gray-900"
-								onClick={toggleOverlay}
+								<GiHamburger />
+							</motion.li>
+							<motion.li
+								variants={itemVariants}
+								className="col-span-3 row-span-2"
 							>
-								About
-							</Link>
-						</li>
-						<li className="col-span-2 row-span-3">
-							<Link
-								href="projects"
-								className="p-4 flex justify-end items-start w-full h-full border hover:bg-gray-900"
-								onClick={toggleOverlay}
-							>
-								Projects
-							</Link>
-						</li>
-						<li
-							className="flex justify-center items-center border cursor-pointer hover:bg-gray-900"
-							onClick={toggleOverlay}
-						>
-							<GiHamburger />
-						</li>
-						<li className="col-span-3 row-span-2">
-							<Link
-								href="/skills"
-								className="p-4 flex justify-start items-start w-full h-full border hover:bg-gray-900"
-								onClick={toggleOverlay}
-							>
-								Skills
-							</Link>
-						</li>
-					</ul>
-				</div>
-			)}
+								<Link
+									href="/skills"
+									className="p-4 flex justify-start items-start w-full h-full border bg-gray-800 hover:bg-gray-900"
+									onClick={toggleOverlay}
+								>
+									Skills
+								</Link>
+							</motion.li>
+						</motion.ul>
+					</motion.div>
+				)}
+			</AnimatePresence>
 		</nav>
 	);
 };
