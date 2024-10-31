796bb25e40e3b041e1db060855b2b0f2626e7361
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-10-30 23:58:28 -0700

    create projects section with bento grid

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,9 +1,41 @@
 export default function Projects() {
 	return (
-		<main className="min-h-screen flex justify-center items-center">
-			<div className="text-center">
+		<main className="min-h-screen flex flex-col gradient-bg text-white">
+			<header className="text-center my-4">
 				<h1 className="text-3xl uppercase whitespace-nowrap">Projects</h1>
-			</div>
+			</header>
+			<section className="container w-full flex-grow flex flex-col md:flex-row gap-1 md:gap-2 mb-4 px-1">
+				{/* first container  */}
+				<div className="flex flex-1 flex-row md:flex-col gap-1 md:gap-2">
+					<div className="flex justify-center align-middle flex-1 rounded-2xl glass-grid-bg">
+						<h2>CodeAgainstCancer</h2>
+					</div>
+					<div className="flex justify-center align-middle flex-1 md:flex-2 rounded-2xl glass-grid-bg">
+						<h2>SoloLevel</h2>
+					</div>
+				</div>
+				{/* second container  */}
+				<div className="flex flex-2 md:flex-1.5 flex-col gap-1 md:gap-2">
+					<div className="flex justify-center align-middle flex-1 rounded-2xl glass-grid-bg">
+						<h2>BeBeauty</h2>
+					</div>
+					<div className="flex justify-center align-middle flex-1 rounded-2xl glass-grid-bg">
+						<h2>Portfolio Mio</h2>
+					</div>
+				</div>
+				{/* third container */}
+				<div className="flex flex-1 flex-row md:flex-col gap-1 md:gap-2">
+					<div className="flex justify-center align-middle flex-1.5 md:flex-2 rounded-2xl glass-grid-bg">
+						<h2>PAC-MAN</h2>
+					</div>
+					<div className="flex justify-center align-middle flex-1.5 md:flex-2 rounded-2xl glass-grid-bg">
+						<h2>HIKKEN</h2>
+					</div>
+					<div className="flex justify-center align-middle flex-1 rounded-2xl glass-grid-bg">
+						<h2>MORE</h2>
+					</div>
+				</div>
+			</section>
 		</main>
 	);
 }
