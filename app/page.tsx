d35d808ea444be60d5c869b11817ec00824c2dda
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-11-14 23:42:25 -0800

    make title component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import Hero from "@/components/hero/Hero";
+import Title from "@/components/hero/Title";
 import TypingEffect from "@/components/hero/TypingEffect";
 
 export default function Home() {
 	return (
 		<main className="min-h-screen flex justify-center items-center gradient-bg-purple">
 			<div className="text-center z-50 select-none pointer-events-none">
-				<h1 className="text-5xl md:text-7xl uppercase font-black">
-					Edwin Peraza
-				</h1>
+				<Title>Edwin Peraza</Title>
 				<p className="text-base font-bold text-gray-300 uppercase whitespace-nowrap">
 					<TypingEffect
 						words={[
---
+++ components/hero/Title.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { ReactNode } from "react";
+import { motion } from "framer-motion";
+
+export default function Title({ children }: { children: ReactNode }) {
+	return (
+		<motion.h1
+			initial={{ opacity: 0, y: 20 }}
+			animate={{ opacity: 1, y: 0 }}
+			transition={{ duration: 1 }}
+			className="text-5xl md:text-7xl uppercase font-black"
+		>
+			{children}
+		</motion.h1>
+	);
+}
