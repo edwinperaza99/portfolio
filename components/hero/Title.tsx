ce1f3e3d262788622dfcd1719a2d0f6a5cb4bf02
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-11-16 14:25:33 -0800

    add basic glitch animation

--- components/hero/Title.tsx
+++ components/hero/Title.tsx
@@ -8,7 +8,8 @@ export default function Title({ children }: { children: ReactNode }) {
 			initial={{ opacity: 0, y: 20 }}
 			animate={{ opacity: 1, y: 0 }}
 			transition={{ duration: 1 }}
-			className="text-5xl md:text-7xl uppercase font-black"
+			className="text-5xl md:text-7xl uppercase font-black glitch"
+			data-text={children}
 		>
 			{children}
 		</motion.h1>
