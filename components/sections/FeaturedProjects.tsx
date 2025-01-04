94a30a14c133b0d37238f57526e2ae2e20a7d771
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2025-01-03 18:55:39 -0800

    fix image responsiveness

--- components/sections/FeaturedProjects.tsx
+++ components/sections/FeaturedProjects.tsx
@@ -8,10 +8,9 @@ export default function FeaturedProjects() {
 				<h1 className="text-4xl uppercase">Projects</h1>
 			</header>
 			<section className="w-full grid gap-4 grid-cols-[repeat(auto-fit,_minmax(300px,_1fr))] md:grid-cols-3">
-				{/* Example projects */}
-				<article className="flex flex-col rounded-2xl glass overflow-hidden group">
-					<Link href="/projects/4">
-						<div className="relative aspect-[4/3]">
+				<article className="rounded-2xl glass overflow-hidden group">
+					<Link href="/projects/4" className="flex flex-col h-full">
+						<div className="relative aspect-[4/3] md:aspect-none md:flex-grow overflow-hidden">
 							<Image
 								src="/CodeAgainstCancer/CaC_thumbnail.png"
 								alt="Code Against Cancer"
@@ -19,17 +18,20 @@ export default function FeaturedProjects() {
 								className="object-cover transition-transform duration-300 ease-in-out group-hover:scale-105"
 							/>
 						</div>
-						<div className="p-4 bg-black bg-opacity-50">
-							<h2 className="text-xl font-bold">Code Against Cancer</h2>
-							<p className="text-sm">Django | Bootstrap | AWS | Docker</p>
+						<div className="p-4 bg-black bg-opacity-50 h-20">
+							<h2 className="text-xl md:text-base lg:text-xl font-bold">
+								Code Against Cancer
+							</h2>
+							<p className="text-sm md:text-xs lg:text-sm">
+								Django | Bootstrap | AWS | Docker
+							</p>
 						</div>
 					</Link>
 				</article>
 
-				{/* SoloLevel: Larger card */}
-				<article className="flex flex-col rounded-2xl glass overflow-hidden group md:col-span-2 md:row-span-2">
-					<Link href="/projects/2">
-						<div className="relative aspect-[4/3]">
+				<article className="rounded-2xl glass overflow-hidden group md:col-span-2 md:row-span-2">
+					<Link href="/projects/2" className="flex flex-col h-full">
+						<div className="relative aspect-[4/3] md:aspect-none md:flex-grow overflow-hidden">
 							<Image
 								src="/SoloLevel/SoloLevel_thumbnail_2.png"
 								alt="SoloLevel"
@@ -37,16 +39,20 @@ export default function FeaturedProjects() {
 								className="object-cover transition-transform duration-300 ease-in-out group-hover:scale-105"
 							/>
 						</div>
-						<div className="p-4 bg-black bg-opacity-50">
-							<h2 className="text-xl font-bold">SoloLevel</h2>
-							<p className="text-sm">Swift | Swift UI | Firebase</p>
+						<div className="p-4 bg-black bg-opacity-50 h-20">
+							<h2 className="text-xl md:text-base lg:text-xl font-bold">
+								SoloLevel
+							</h2>
+							<p className="text-sm md:text-xs lg:text-sm">
+								Swift | Swift UI | Firebase
+							</p>
 						</div>
 					</Link>
 				</article>
 
-				<article className="flex flex-col rounded-2xl glass overflow-hidden group">
-					<Link href="/projects/1">
-						<div className="relative aspect-[4/3]">
+				<article className="rounded-2xl glass overflow-hidden group">
+					<Link href="/projects/1" className="flex flex-col h-full">
+						<div className="relative aspect-[4/3] md:aspect-none md:flex-grow overflow-hidden">
 							<Image
 								src="/NataliaSalon/NataliaSalon_thumbnail.png"
 								alt="NataliaSalon project"
@@ -54,16 +60,20 @@ export default function FeaturedProjects() {
 								className="object-cover transition-transform duration-300 ease-in-out group-hover:scale-105"
 							/>
 						</div>
-						<div className="p-4 bg-black bg-opacity-50">
-							<h2 className="text-xl font-bold">BeBeauty</h2>
-							<p className="text-sm">Next.js | Tailwind | MongoDB</p>
+						<div className="p-4 bg-black bg-opacity-50 h-20">
+							<h2 className="text-xl md:text-base lg:text-xl font-bold">
+								BeBeauty
+							</h2>
+							<p className="text-sm md:text-xs lg:text-sm">
+								Next.js | Tailwind | MongoDB
+							</p>
 						</div>
 					</Link>
 				</article>
 
-				<article className="flex flex-col rounded-2xl glass overflow-hidden group md:col-span-2 md:row-span-2">
-					<Link href="/projects/3">
-						<div className="relative aspect-[4/3]">
+				<article className="rounded-2xl glass overflow-hidden group md:col-span-2 md:row-span-2">
+					<Link href="/projects/3" className="flex flex-col h-full">
+						<div className="relative aspect-[4/3] md:aspect-none md:flex-grow overflow-hidden">
 							<Image
 								src="/MioPortfolio/MioPortfolio_thumbnail.png"
 								alt="Portfolio Mio"
@@ -71,16 +81,20 @@ export default function FeaturedProjects() {
 								className="object-cover transition-transform duration-300 ease-in-out group-hover:scale-105"
 							/>
 						</div>
-						<div className="p-4 bg-black bg-opacity-50">
-							<h2 className="text-xl font-bold">Portfolio Mio</h2>
-							<p className="text-sm">Next.js | Tailwind CSS</p>
+						<div className="p-4 bg-black bg-opacity-50 h-20">
+							<h2 className="text-xl md:text-base lg:text-xl font-bold">
+								Portfolio Mio
+							</h2>
+							<p className="text-sm md:text-xs lg:text-sm">
+								Next.js | Tailwind CSS
+							</p>
 						</div>
 					</Link>
 				</article>
 
-				<article className="flex flex-col rounded-2xl glass overflow-hidden group">
-					<Link href="/projects/6">
-						<div className="relative aspect-[4/3]">
+				<article className="rounded-2xl glass overflow-hidden group">
+					<Link href="/projects/6" className="flex flex-col h-full">
+						<div className="relative aspect-[4/3] md:aspect-none md:flex-grow overflow-hidden">
 							<Image
 								src="/PAC-MAN/pacman_thumbnail_2.png"
 								alt="PAC-MAN"
@@ -88,16 +102,18 @@ export default function FeaturedProjects() {
 								className="object-cover transition-transform duration-300 ease-in-out group-hover:scale-105"
 							/>
 						</div>
-						<div className="p-4 bg-black bg-opacity-50">
-							<h2 className="text-xl font-bold">PAC-MAN</h2>
-							<p className="text-sm">Python | Pygame</p>
+						<div className="p-4 bg-black bg-opacity-50 h-20">
+							<h2 className="text-xl md:text-base lg:text-xl font-bold">
+								PAC-MAN
+							</h2>
+							<p className="text-sm md:text-xs lg:text-sm">Python | Pygame</p>
 						</div>
 					</Link>
 				</article>
 
-				<article className="flex flex-col rounded-2xl glass overflow-hidden group">
-					<Link href="/projects/8">
-						<div className="relative aspect-[4/3]">
+				<article className="rounded-2xl glass overflow-hidden group">
+					<Link href="/projects/8" className="flex flex-col h-full">
+						<div className="relative aspect-[4/3] md:aspect-none md:flex-grow overflow-hidden">
 							<Image
 								src="/Hikken/Hikken_thumbnail.png"
 								alt="Hikken"
@@ -105,9 +121,13 @@ export default function FeaturedProjects() {
 								className="object-cover transition-transform duration-300 ease-in-out group-hover:scale-105"
 							/>
 						</div>
-						<div className="p-4 bg-black bg-opacity-50">
-							<h2 className="text-xl font-bold">Hikken</h2>
-							<p className="text-sm">HTML | CSS | JavaScript</p>
+						<div className="p-4 bg-black bg-opacity-50 h-20">
+							<h2 className="text-xl md:text-base lg:text-xl font-bold">
+								Hikken
+							</h2>
+							<p className="text-sm md:text-xs lg:text-sm">
+								HTML | CSS | JavaScript
+							</p>
 						</div>
 					</Link>
 				</article>
