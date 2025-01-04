a6800c5d9f664aad0fda0045f2e3947d94201143
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2025-01-03 20:02:04 -0800

    update skills section

--- components/sections/Skills.tsx
+++ components/sections/Skills.tsx
@@ -10,54 +10,53 @@ import {
 
 export default function Skills() {
 	return (
-		<div className="min-h-screen flex flex-col text-primary-light">
-			<header className="text-center my-4">
-				<h1 className="text-3xl uppercase whitespace-nowrap">Skills</h1>
+		<div className="container py-10 flex flex-col text-primary-light px-4">
+			<header className="my-4">
+				<h1 className="text-4xl uppercase whitespace-nowrap">Skills</h1>
 			</header>
-			{skillCategories.map((category) => (
-				<section
-					key={category.id}
-					className="container w-full flex flex-col gap-1 md:gap-2 mb-4 px-2"
-				>
-					<h2 className="text-2xl uppercase whitespace-nowrap">
-						{category.name}
-					</h2>
-					<article className="flex flex-wrap gap-2 md:gap-4">
-						{resolveSkills(category.skills).map(
-							(skill) =>
-								skill && (
-									<TooltipProvider key={skill.id}>
-										<Tooltip>
-											<TooltipTrigger>
-												<div
-													key={skill.id}
-													className="skill p-4 flex gap-3 glass rounded-full"
-													// style={{ backgroundColor: skill.color }}
-													style={
-														{
+			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+				{skillCategories.map((category) => (
+					<section
+						key={category.id}
+						className="flex flex-col gap-2 rounded-lg glass p-4 shadow-md"
+					>
+						<h2 className="text-xl uppercase whitespace-nowrap text-center mb-4">
+							{category.name}
+						</h2>
+						<article className="flex flex-wrap gap-2 md:gap-4 justify-center">
+							{resolveSkills(category.skills).map(
+								(skill) =>
+									skill && (
+										<TooltipProvider key={skill.id}>
+											<Tooltip>
+												<TooltipTrigger>
+													<div
+														key={skill.id}
+														className="skill p-4 flex gap-3 glass rounded-full"
+														style={{
 															"--skill-color": skill.color,
-														} as React.CSSProperties
-													}
-												>
-													<Image
-														src={skill.icon}
-														alt={`${skill.name} icon`}
-														width={30}
-														height={30}
-														className="drop-shadow-3xl"
-													/>
-												</div>
-											</TooltipTrigger>
-											<TooltipContent>
-												<p>{skill.name}</p>
-											</TooltipContent>
-										</Tooltip>
-									</TooltipProvider>
-								)
-						)}
-					</article>
-				</section>
-			))}
+														}}
+													>
+														<Image
+															src={skill.icon}
+															alt={`${skill.name} icon`}
+															width={30}
+															height={30}
+															className="drop-shadow-3xl"
+														/>
+													</div>
+												</TooltipTrigger>
+												<TooltipContent>
+													<p>{skill.name}</p>
+												</TooltipContent>
+											</Tooltip>
+										</TooltipProvider>
+									)
+							)}
+						</article>
+					</section>
+				))}
+			</div>
 		</div>
 	);
 }
