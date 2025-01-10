033b9ea57d08442a376395e90bcefcc8bbf47e50
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2025-01-09 23:58:02 -0800

    add lottie

---
+++ components/lottie/Scroll.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { Player } from "@lottiefiles/react-lottie-player";
+
+export default function Scroll() {
+	return (
+		<Player
+			autoplay
+			loop
+			src="https://lottie.host/c0ced8fe-ea31-462d-ab2b-fdb3958d343c/qx2hoFqKqI.json"
+			background="transparent"
+			style={{ height: "75px", width: "75px" }}
+		/>
+	);
+}
